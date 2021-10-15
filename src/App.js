d29ffbfa11a454da03fddcf4405aa3c7e769d3b6
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2021-10-16 00:56:51 +0600

    done tic-tac-toe

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import { useState } from 'react';
-
-function App() {
-  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
-  return <div className="App"></div>;
-}
-
-export default App;
